c707ee5b04d2300bb648ad83fd50dc7bfc1db87a
robtomatkins <robtomatkins@outlook.com>
2022-08-02 18:09:53 +0000

    add console.log to test if game function works

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,55 @@ const scissors_div = document.getElementById("scissors")
 const lizard_div = document.getElementById("lizard")
 const spock_div = document.getElementById("spock")
 
-
-function compChoice() {
-    /* computer choice array - chooses either rock, paper, scissors, lizard or spock from the array*/
-    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+/* computer choice array - chooses either rock, paper, scissors, lizard or spock from the array*/
+function getCompChoice() {
+    const choices = ['r', 'p', 'sc', 'l', 'sp'];
     const randomNumber = Math.floor(Math.random() * 5);
-    return choices(randomNumber);
-
+    return choices[randomNumber];
+    
 }
 
-/* game function is called when rock_div is clicked*/
+
+
+/* game function is called when rock_div is clicked */
 function game(playerChoice) {
-     
+     const compChoice = getCompChoice();
+     switch (playerChoice + compChoice) {
+        case "rl":
+        case "scl":
+        case "lp":
+        case "lsp":
+        case "psp":
+        case "spr":
+        case "spsc":
+        case "rs":
+        case "pr":
+        case "scp":
+            console.log("You Win!");
+            break;
+        case "lr":
+        case "lsc":
+        case "pl":
+        case "spl":
+        case "spp":
+        case "rsp":
+        case "scsp":
+        case "sr":
+        case "rp":
+        case "psc":
+            console.log("You lose!");
+            break;
+        case "rr":
+        case "pp":
+        case "scsc":
+        case "spsp":
+        case "ll":
+            console.log("It's a draw!")
+            break;
+     }
 }
 
 function runGame() {
-
 rock_div.addEventListener('click', function() {
     game("r"); 
 })
