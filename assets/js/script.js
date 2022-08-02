c4f6e6323575ce2383a16f9f1fba5d28d71d4c85
robtomatkins <robtomatkins@outlook.com>
2022-08-02 16:28:05 +0000

    add event listeners for control buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
-let playerScore = 0;
-let compScore = 0;
-let playerScore_span = document.getElementById("player-score");
-let compScore_span = document.getElementById("comp-score");
-let scoreBoard_div = document.getElementById("score-board");
-let gameMessage_div = document.getElementById("game-message")
-let rock_div = document.getElementById("rock")
-let paper_div = document.getElementById("paper")
-let scissors_div = document.getElementById("scissors")
-let lizard_div = document.getElementById("lizard")
-let spock_div = document.getElementById("spock")
+const playerScore = 0;
+const compScore = 0;
+const playerScore_span = document.getElementById("player-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.getElementById("score-board");
+const gameMessage_div = document.getElementById("game-message")
+const rock_div = document.getElementById("rock")
+const paper_div = document.getElementById("paper")
+const scissors_div = document.getElementById("scissors")
+const lizard_div = document.getElementById("lizard")
+const spock_div = document.getElementById("spock")
+
+/* Event listener for control buttons */
+
+rock_div.addEventListener('click', function() {
+    console.log("you clicked on rock"); 
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("you clicked on rock"); 
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log("you clicked on rock"); 
+})
+
+lizard_div.addEventListener('click', function() {
+    console.log("you clicked on rock"); 
+})
+
+spock_div.addEventListener('click', function() {
+    console.log("you clicked on rock"); 
+})
